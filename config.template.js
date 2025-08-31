4dcc1b42d8359fe8b17adf443011e2a1d071daaa
Njanyana Jaytee Xaba <njanyanaxaba5@gmail.com>
2025-08-31 05:37:10 +0200

    Add Appwrite configuration template

---
+++ config.template.js
@@ -0,0 +1,12 @@
+// TEMPLATE FILE - Copy to config.js and fill in your own values
+const APPWRITE_CONFIG = {
+    endpoint: 'https://cloud.appwrite.io/v1',
+    projectId: 'YOUR_PROJECT_ID',
+    databaseId: 'YOUR_DATABASE_ID',
+    collections: {
+        users: 'users',
+        modules: 'modules',
+        assignments: 'assignments',
+        grades: 'grades'
+    }
+};

763cd024196a3e8b25c5285cd3783c800cc3cd47
Njanyana Jaytee Xaba <njanyanaxaba5@gmail.com>
2025-08-31 05:34:47 +0200

    Add secure Appwrite configuration file

---
+++ config.js
@@ -0,0 +1,12 @@
+// SECURE CONFIG - This file should be in .gitignore
+const APPWRITE_CONFIG = {
+    endpoint: 'https://cloud.appwrite.io/v1',
+    projectId: 'fra-68b39c1800ae7d1e3ac',
+    databaseId: '68b3a376002c8b02b071',
+    collections: {
+        users: 'users',
+        modules: 'modules',
+        assignments: 'assignments',
+        grades: 'grades'
+    }
+};
